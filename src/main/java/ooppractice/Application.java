577bc90b96aa2c8c1687236dc37119c5e387766d
zooneon <snsxhdz@naver.com>
2022-02-20 01:18:04 +0900

    feat: 메인 애플리케이션 생성

--- src/main/java/ooppractice/Application.java
+++ src/main/java/ooppractice/Application.java
@@ -1,2 +1,9 @@
-package ooppractice;public class Application {
+package ooppractice;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+    }
 }
