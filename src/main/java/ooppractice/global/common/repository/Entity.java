0be1b943a52c3f70d99a13a8cf182a9894a4cefe
zooneon <snsxhdz@naver.com>
2022-02-22 00:05:57 +0900

    fix: getter 추가

--- src/main/java/ooppractice/global/common/repository/Entity.java
+++ src/main/java/ooppractice/global/common/repository/Entity.java
@@ -1,8 +1,10 @@
 package ooppractice.global.common.repository;
 
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.Setter;
 
+@Getter
 @Setter(AccessLevel.PROTECTED)
 public class Entity {
 
