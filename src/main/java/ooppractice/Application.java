91c9473dba61b199d47797ae7429d37607e19f9f
zooneon <snsxhdz@naver.com>
2022-02-24 15:59:04 +0900

    fix: 메인 application 수정

--- src/main/java/ooppractice/Application.java
+++ src/main/java/ooppractice/Application.java
@@ -1,13 +1,9 @@
 package ooppractice;
 
-import ooppractice.global.common.exception.ErrorCode;
-import ooppractice.global.common.exception.ErrorResponse;
-
 import java.util.Scanner;
 
-public class Main {
+public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        System.out.println(ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE));
     }
 }
