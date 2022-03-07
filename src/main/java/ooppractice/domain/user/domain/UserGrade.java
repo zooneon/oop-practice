aea81fb49619165f16b262c07923cca8ebd6098b
zooneon <snsxhdz@naver.com>
2022-03-08 01:40:55 +0900

    fix: 상수 수정

--- src/main/java/ooppractice/domain/user/domain/UserGrade.java
+++ src/main/java/ooppractice/domain/user/domain/UserGrade.java
@@ -9,9 +9,9 @@ import java.util.function.Function;
 @Getter
 @RequiredArgsConstructor
 public enum UserGrade {
-    SILVER("SILVER", amount -> amount + (amount * Constant.SILVER_BENEFIT), Constant.SILVER_STANDARD),
-    GOLD("GOLD", amount -> amount + (amount * Constant.GOLD_BENEFIT), Constant.GOLD_STANDARD),
-    PLATINUM("PLATINUM", amount -> amount + (amount * Constant.PLATINUM_BENEFIT), Constant.PLATINUM_STANDARD);
+    SILVER("SILVER", amount -> amount + (amount * 0.03), 0),
+    GOLD("GOLD", amount -> amount + (amount * 0.05), 50000),
+    PLATINUM("PLATINUM", amount -> amount + (amount * 0.1), 100000);
 
     private final String grade;
     private final Function<Integer, Double> expression;
--- src/main/java/ooppractice/global/util/Constant.java
+++ src/main/java/ooppractice/global/util/Constant.java
@@ -6,22 +6,23 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class Constant {
 
-    //user grade
-    public static final double SILVER_BENEFIT = 0.03;
-    public static final double GOLD_BENEFIT = 0.05;
-    public static final double PLATINUM_BENEFIT = 0.1;
-    public static final int SILVER_STANDARD = 0;
-    public static final int GOLD_STANDARD = 50000;
-    public static final int PLATINUM_STANDARD = 100000;
-
-    //payment type
-    public static final double N_PAY_BENEFIT = 0.05;
-    public static final double BM_PAY_BENEFIT = 0.04;
-    public static final double KA_PAY_BENEFIT = 0.03;
+    //deposit
+    public static final int LOWER_LIMIT_OF_DEPOSIT_AMOUNT = 1000;
 
     //view
+    public static final String DOT = ".";
     public static final String BLANK = " ";
     public static final String SEMICOLON = ":";
     public static final String NEXT_LINE = "\n";
     public static final String ERROR_CODE = "에러코드";
+    public static final int SB_START_INDEX = 0;
+    public static final String MENU_INPUT_MESSAGE = "[실행하고자 하는 메뉴의 번호를 입력하세요]";
+    public static final int OPTION_ONE = 1;
+    public static final int OPTION_TWO = 2;
+    public static final int OPTION_THREE = 3;
+    public static final int OPTION_FOUR = 4;
+    public static final int OPTION_FIVE = 5;
+    public static final String QUIT = "나가기";
+    public static final String WON = "원";
+    public static final String BOUNDARY = "---------------------";
 }
