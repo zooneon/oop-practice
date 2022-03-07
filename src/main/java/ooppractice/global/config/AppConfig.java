de85f6e9a02a03a1c59f4e69dbd600cfdab156e9
zooneon <snsxhdz@naver.com>
2022-03-08 01:37:39 +0900

    feat: view 관련 설정 추가

--- src/main/java/ooppractice/global/config/AppConfig.java
+++ src/main/java/ooppractice/global/config/AppConfig.java
@@ -22,6 +22,24 @@ import ooppractice.domain.user.repository.UserRepository;
 import ooppractice.domain.user.service.UserService;
 import ooppractice.domain.user.service.UserServiceImpl;
 import ooppractice.global.util.GetLocalDateTime;
+import ooppractice.view.LoginView;
+import ooppractice.view.MainView;
+import ooppractice.view.mypage.MyPageDepositView;
+import ooppractice.view.mypage.MyPageInfoView;
+import ooppractice.view.mypage.MyPageMainView;
+import ooppractice.view.order.OrderCancelView;
+import ooppractice.view.order.OrderInfoView;
+import ooppractice.view.order.OrderMainView;
+import ooppractice.view.order.OrderView;
+import ooppractice.view.payment.PaymentCancelView;
+import ooppractice.view.payment.PaymentInfoView;
+import ooppractice.view.payment.PaymentMainView;
+import ooppractice.view.payment.PaymentView;
+import ooppractice.view.search.CategoryNameSearchView;
+import ooppractice.view.search.ItemNameSearchView;
+import ooppractice.view.search.SearchMainView;
+
+import java.util.Scanner;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -53,10 +71,38 @@ public class AppConfig {
 
     //util
     private static GetLocalDateTime getLocalDateTime;
+    private static Scanner scanner;
+
+    //view
+    private static LoginView loginView;
+    private static MainView mainView;
+
+    //mypage view
+    private static MyPageMainView myPageMainView;
+    private static MyPageInfoView myPageInfoView;
+    private static MyPageDepositView myPageDepositView;
+
+    //order view
+    private static OrderMainView orderMainView;
+    private static OrderView orderView;
+    private static OrderInfoView orderInfoView;
+    private static OrderCancelView orderCancelView;
+
+    //payment view
+    private static PaymentMainView paymentMainView;
+    private static PaymentView paymentView;
+    private static PaymentInfoView paymentInfoView;
+    private static PaymentCancelView paymentCancelView;
+
+    //search view
+    private static SearchMainView searchMainView;
+    private static ItemNameSearchView itemNameSearchView;
+    private static CategoryNameSearchView categoryNameSearchView;
 
     public static void configure() {
         //util
         getLocalDateTime = new GetLocalDateTime();
+        scanner = new Scanner(System.in);
         //user
         userRepository = new UserRepository();
         userService = new UserServiceImpl(userRepository);
@@ -75,6 +121,27 @@ public class AppConfig {
         //payment
         paymentRepository = new PaymentRepository();
         paymentService = new PaymentServiceImpl(paymentRepository, orderService, getLocalDateTime);
+        //view
+        loginView = new LoginView(scanner, userService);
+        mainView = new MainView(scanner);
+        //mypage view
+        myPageMainView = new MyPageMainView(scanner);
+        myPageInfoView = new MyPageInfoView(scanner, userService);
+        myPageDepositView = new MyPageDepositView(scanner, userService);
+        //order view
+        orderMainView = new OrderMainView(scanner);
+        orderView = new OrderView(scanner, orderService);
+        orderInfoView = new OrderInfoView(scanner, orderService);
+        orderCancelView = new OrderCancelView(scanner, orderService);
+        //payment view
+        paymentMainView = new PaymentMainView(scanner);
+        paymentView = new PaymentView(scanner, paymentService);
+        paymentInfoView = new PaymentInfoView(scanner, paymentService);
+        paymentCancelView = new PaymentCancelView(scanner, paymentService);
+        //search view
+        searchMainView = new SearchMainView(scanner);
+        itemNameSearchView = new ItemNameSearchView(scanner);
+        categoryNameSearchView = new CategoryNameSearchView(scanner);
     }
 
     public static UserRepository getUserRepository() {
@@ -127,5 +194,73 @@ public class AppConfig {
 
     public static GetLocalDateTime getGetLocalDateTime() {
         return getLocalDateTime;
+    }
+
+    public static Scanner getScanner() {
+        return scanner;
+    }
+
+    public static LoginView getLoginView() {
+        return loginView;
+    }
+
+    public static MainView getMainView() {
+        return mainView;
+    }
+
+    public static MyPageMainView getMyPageMainView() {
+        return myPageMainView;
+    }
+
+    public static MyPageInfoView getMyPageInfoView() {
+        return myPageInfoView;
+    }
+
+    public static MyPageDepositView getMyPageDepositView() {
+        return myPageDepositView;
+    }
+
+    public static OrderMainView getOrderMainView() {
+        return orderMainView;
+    }
+
+    public static OrderView getOrderView() {
+        return orderView;
+    }
+
+    public static OrderInfoView getOrderInfoView() {
+        return orderInfoView;
+    }
+
+    public static OrderCancelView getOrderCancelView() {
+        return orderCancelView;
+    }
+
+    public static PaymentMainView getPaymentMainView() {
+        return paymentMainView;
+    }
+
+    public static PaymentView getPaymentView() {
+        return paymentView;
+    }
+
+    public static PaymentInfoView getPaymentInfoView() {
+        return paymentInfoView;
+    }
+
+    public static PaymentCancelView getPaymentCancelView() {
+        return paymentCancelView;
+    }
+
+    public static SearchMainView getSearchMainView() {
+        return searchMainView;
+    }
+
+    public static ItemNameSearchView getItemNameSearchView() {
+        return itemNameSearchView;
+    }
+
+    public static CategoryNameSearchView getCategoryNameSearchView() {
+        return categoryNameSearchView;
     }
 }
