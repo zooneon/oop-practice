5cd563abe5507fb0a5f25cd4b316f4789129beff
zooneon <snsxhdz@naver.com>
2022-03-09 16:23:53 +0900

    fix: 혜택 비율 추가

--- src/main/java/ooppractice/domain/payment/domain/PaymentType.java
+++ src/main/java/ooppractice/domain/payment/domain/PaymentType.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import ooppractice.domain.payment.exception.PaymentTypeNotFoundException;
 import ooppractice.global.exception.ErrorCode;
-import ooppractice.global.util.Constant;
 
 import java.util.Arrays;
 import java.util.function.Function;
@@ -12,11 +11,12 @@ import java.util.function.Function;
 @Getter
 @RequiredArgsConstructor
 public enum PaymentType {
-    N_PAY("NPay", amount -> amount - (amount * 0.05)),
-    KA_PAY("KaPay", amount -> amount - (amount * 0.03)),
-    BM_PAY("BMPay", amount -> amount - (amount * 0.04));
+    N_PAY("NPay", 0.05, amount -> amount - (amount * 0.05)),
+    KA_PAY("KaPay", 0.03, amount -> amount - (amount * 0.03)),
+    BM_PAY("BMPay", 0.04, amount -> amount - (amount * 0.04));
 
     private final String name;
+    private final Double benefitRatio;
     private final Function<Integer, Double> expression;
 
     public int calculateBenefit(int amount) {
--- src/main/java/ooppractice/domain/user/domain/UserGrade.java
+++ src/main/java/ooppractice/domain/user/domain/UserGrade.java
@@ -2,18 +2,18 @@ package ooppractice.domain.user.domain;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import ooppractice.global.util.Constant;
 
 import java.util.function.Function;
 
 @Getter
 @RequiredArgsConstructor
 public enum UserGrade {
-    SILVER("SILVER", amount -> amount + (amount * 0.03), 0),
-    GOLD("GOLD", amount -> amount + (amount * 0.05), 50000),
-    PLATINUM("PLATINUM", amount -> amount + (amount * 0.1), 100000);
+    SILVER("SILVER", 0.03, amount -> amount + (amount * 0.03), 0),
+    GOLD("GOLD", 0.05, amount -> amount + (amount * 0.05), 50000),
+    PLATINUM("PLATINUM", 0.1, amount -> amount + (amount * 0.1), 100000);
 
     private final String grade;
+    private final Double benefitRatio;
     private final Function<Integer, Double> expression;
     private final int standard;
 
